c31e35309ef1282d716b6b880a4cac82299fb01a
Will Maier <willmaier@ml1.net>
2011-09-09 09:28:11 -0500

    Use curl to read from the network.
    Fixes cutoffs midstream.

--- play.sh
+++ play.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+UA="Mozilla/5.0"
+CURL="curl -s --user-agent \'${UA}\' -L"
+
 while :; do 
-    mplayer -cache 8192 -really-quiet $(./streamr.py pop)
+    ${CURL} $(./streamr.py pop) | mplayer -cache 8192 -really-quiet -
     sleep 1
 done
